e96f090da97f134721f5131223b9525149bcfa67
schmeckm <markus.schmeckenbecher@gmail.com>
2023-06-18 13:06:59 +0200

    Version 1

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,18 +1,17 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-dotenv.config({ path: '.env' });
-console.log('MongoDB URI:', process.env.MongoDB_URI);
+dotenv.config();
 
 mongoose.set('strictQuery', false);
 
 const dbConn = async () => {
   try {
-    await mongoose.connect(MongoDB_URI, {
+    await mongoose.connect(process.env.MongoDB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('Database is connected successfully');
+    console.log('Database is connected successfully to:', process.env.MongoDB_URI);
   } catch (error) {
     console.log('Error connecting to the database:', error.message);
   }
