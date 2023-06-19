3ccb8e60c2736fd566ef5ba8c5192185d822f549
schmeckm <markus.schmeckenbecher@gmail.com>
2023-06-19 18:11:32 +0200

    Update_Zone

---
+++ controllers/zoneController.js
@@ -0,0 +1,149 @@
+const Zone_Id = require('../models/Zones');
+
+exports.getAllZones = async (req, res) => {
+
+	var options = {
+		allowDiskUse: true
+	};
+
+	var pipeline = [
+		{
+			"$project": {
+				"_id": 0,
+				"zone_ids": "$$ROOT"
+			}
+		},
+		{
+			"$lookup": {
+				"localField": "zone_ids.environment",
+				"from": "environments",
+				"foreignField": "_id",
+				"as": "environments"
+			}
+		},
+		{
+			"$unwind": {
+				"path": "$environments",
+				"preserveNullAndEmptyArrays": false
+			}
+		},
+		{
+			"$project": {
+				"zone_ids.zoneId": "$zone_ids.zoneId",
+				"zone_ids.description": "$zone_ids.description",
+				"zone_ids.text1": "$zone_ids.text1",
+				"zone_ids.text2": "$zone_ids.text2",
+				"environments.description": "$environments.description",
+				"_id": 0
+			}
+		}
+	];
+
+	try {
+
+		const zones = await Zone_Id.aggregate(pipeline, options);
+		res.status(200).json({
+			success: true,
+			data: zones,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
+
+exports.addZone = async (req, res) => {
+	try {
+		const zone = await Zone_Id.create(req.body);
+		res.status(200).json({
+			success: true,
+			data: zone,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
+
+exports.deleteZone = async (req, res) => {
+	try {
+		c
+
+		const zone = await Zone_Id.findOne({ _id: zoneId });
+
+		if (!zone) {
+			return res.status(404).json({
+				success: false,
+				error: 'Zone not found',
+			});
+		}
+
+		await Zone_Id.findOneAndDelete({ _id: zoneId });
+
+		res.status(200).json({
+			success: true,
+			data: 'Zone Deleted Successfully',
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
+
+exports.updateZone = async (req, res) => {
+	try {
+		const { zoneId } = req.params;
+
+		const zone = await Zone_Id.findOne({ _id: zoneId });
+
+		if (!zone) {
+			return res.status(404).json({
+				success: false,
+				error: 'Zone not found',
+			});
+		}
+
+		await Zone_Id.findOneAndUpdate({ _id: zoneId }, req.body);
+
+		res.status(200).json({
+			success: true,
+			data: 'Zone Updated Successfully',
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
+
+exports.getSingleZone = async (req, res) => {
+	try {
+		const { zoneId } = req.params;
+
+		const zone = await Zone_Id.findOne({ _id: zoneId });
+
+		if (!zone) {
+			return res.status(404).json({
+				success: false,
+				error: 'Zone not found',
+			});
+		}
+
+		res.status(200).json({
+			success: true,
+			data: zone,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
--- routes/routesConfig.js
+++ routes/routesConfig.js
@@ -3,12 +3,14 @@ const beaconRouter = require('../routes/dbBeaconRoutes');
 const gatewayRouter = require('../routes/gatewayRoutes');
 const environmentRouter = require('../routes/environmentRoutes');
 const positionRouter = require('../routes/PostionRoutes');
+const zoneRouter = require('../routes/zoneRoutes');
 
 const routes = [
   { path: '/api/beacon', router: beaconRouter },
   { path: '/api/gateway', router: gatewayRouter },
   { path: '/api/environment', router: environmentRouter },
   { path: '/api/position', router: positionRouter },
+  { path: '/api/zone', router: zoneRouter },
 ];
 
 module.exports = routes;
